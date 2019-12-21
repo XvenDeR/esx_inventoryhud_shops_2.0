95caff899da9ba8979a38c19eb78afb891143062
GMA950 <francobocca.97@gmail.com>
2019-12-21 20:54:36 -0300

    readme updated

---
+++ esx_inventoryhud/html/locales/es.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "<span class=\"emoji\">🗑️</span> Tirar";
+invLocale.useItem = "<span class=\"emoji\">🖐️</span> Usar";
+invLocale.giveItem = "<span class=\"emoji\">🎁</span> Dar";
+invLocale.secondInventoryNotAvailable = "Inventario secundario no disponible";
